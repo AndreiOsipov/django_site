d0cb3fe0d1706f332e27d5268d6d4e53d717ccdb
andreyka_2082@mail.ru <andreyka_2082@mail.ru>
2022-06-17 15:03:49 +0500

    not work out

--- generator/image_list_generator.py
+++ generator/image_list_generator.py
@@ -1,6 +1,4 @@
 import os
-from ..urfu_project import settings
-
 def generate_image_list(key_word):
     image_name_list = []
     PATH = os.path.dirname(os.path.abspath(__file__))
@@ -8,5 +6,5 @@ def generate_image_list(key_word):
     files = os.listdir(path=path)
     for i in range(len(files)):
         full_path = os.path.join(path, files[i])
-        image_name_list.append(settings.STATIC_ROOT)
+        image_name_list.append(full_path)
     return image_name_list

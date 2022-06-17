1b38c6a15feec7fa511ef156dbab8ce9ce806006
andreyka_2082@mail.ru <andreyka_2082@mail.ru>
2022-06-17 13:44:02 +0500

    local

---
+++ generator/Forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class KeyWordForm(forms.Form):
+    word = forms.CharField(
+        required = False, 
+        max_length=100, 
+        label='', 
+        label_suffix='',
+        widget=forms.TextInput(attrs={"class": "input_field"}))
---
+++ urfu_project/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn urfu_project.wsgi --log-file -

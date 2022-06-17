605e1b38bf20c990eea90d567fc38f11b05a2ad0
andreyka_2082@mail.ru <andreyka_2082@mail.ru>
2022-06-17 21:20:53 +0500

    ho

--- urfu_project/settings.py
+++ urfu_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [

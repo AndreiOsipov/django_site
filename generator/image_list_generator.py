250152a8d644b362d9281d87ec8fda81dbe43834
andreyka_2082@mail.ru <andreyka_2082@mail.ru>
2022-06-17 14:36:03 +0500

    maybebaibe

--- generator/image_list_generator.py
+++ generator/image_list_generator.py
@@ -2,7 +2,7 @@ import os
 def generate_image_list(key_word):
     image_name_list = []
     PATH = os.path.dirname(os.path.abspath(__file__))
-    path = os.join(PATH, 'static/generator/images')
+    path = os.path.join(PATH, 'static/generator/images')
     files = os.listdir(path=path)
     for i in range(len(files)):
         full_path = os.path.join(path, files[i])
